ffaa205626d20cd4ebd9c443aee968429771f07c
Oriana Pardo <opmalzahn@gmail.com>
2022-11-20 20:08:38 -0300

    Prueba42

--- gradle.groovy
+++ gradle.groovy
@@ -1,16 +1,16 @@
-def compile() {
+def grdl_compile() {
     stage('grdl compile'){
         sh 'gradle build'
     }
 }
 
-def run() {
+def grdl_run() {
     stage('grdl run'){
         sh 'gradle build'
     }
 }
 
-def test() {
+def grdl_test() {
     stage('grdl test'){
         sh 'curl -X GET \'http://localhost:8081/rest/mscovid/test?msg=testing\''
     }
